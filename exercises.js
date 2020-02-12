d830490fa1d1c95ca9bd7478a58f52a73dcc73e6
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-02-12 03:23:02 -0500

    Valid Parentheses

--- exercises.js
+++ exercises.js
@@ -4,3 +4,27 @@ const toLowerCase = function (str) {
 
 // console.log(toLowerCase("HELLO"))
 
+const isValid = function (s) {
+    const pairs = {
+        "(": ")",
+        "[": "]",
+        "{": "}"
+    }
+    const stack = []
+
+    for (char of s) {
+        if (char in pairs) {
+            stack.push(char)
+        } else {
+            const lastEntry = stack.pop()
+            if (char !== pairs[lastEntry]) return false
+        }
+    }
+
+    return (stack.length === 0) ? true : false
+};
+// console.log(isValid("()"))
+// console.log(isValid("()[]{}"))
+// console.log(isValid("(]"))
+// console.log(isValid("([)]"))
+// console.log(isValid("{[]}"))
