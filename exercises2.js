0c7670b4410d996f13e870222109edf46e38bb7c
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-03-19 12:43:04 -0400

    linkedlist

--- exercises2.js
+++ exercises2.js
@@ -14,3 +14,15 @@ const k = 3
 const t = 0 // true
 
 console.log(containsNearbyAlmostDuplicate(nums, k, t))
+
+var deleteDuplicates = function (head) {
+    let node = head
+
+    while (node != null && node.next != null)
+        if (node.val == node.next.val)
+            node.next = node.next.next
+        else
+            node = node.next
+
+    return head
+};
