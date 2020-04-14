8c3196cc3fd3eef4d98da78ea86c3844dfb68b05
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-04-13 23:23:57 -0400

    Complete Hash Map

--- exercises3.js
+++ exercises3.js
@@ -4,3 +4,14 @@ var MyHashMap = function () {
     this.map = {}
 };
 
+MyHashMap.prototype.put = function (key, value) {
+    this.map[key] = value
+};
+
+MyHashMap.prototype.get = function (key) {
+    return this.map[key] != null ? this.map[key] : -1
+};
+
+MyHashMap.prototype.remove = function (key) {
+    this.map[key] = null
+};
