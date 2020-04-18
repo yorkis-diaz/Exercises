0a5b3ec01aec0982cc0264cd8c2e0d3177ca3da3
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-04-17 23:37:00 -0400

    RomantoInt

--- exercises3.js
+++ exercises3.js
@@ -44,3 +44,31 @@ var missingNumber = function (nums) {
     let sum = nums.reduce((a, b) => a + b);
     return (n * (n - 1) / 2) - sum;
 };
+
+// romantoInt
+
+var romanToInt = function (s) {
+    var sym = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+
+    let res = 0;
+    for (let i = 0; i < s.length; i++) {
+        if ((s[i] && s[i + 1]) &&
+            (s[i] === 'I' && (s[i + 1] === 'V' || s[i + 1] === 'X')) ||
+            (s[i] === 'X' && (s[i + 1] === 'L' || s[i + 1] === 'C')) ||
+            (s[i] === 'C' && (s[i + 1] === 'D' || s[i + 1] === 'M'))) {
+            res = res - sym[s[i]];
+        }
+        else {
+            res = res + sym[s[i]];
+        }
+    }
+    return res;
+};
