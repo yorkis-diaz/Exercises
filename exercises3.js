849057b44e929f1acb9ac62db68a4fcf39bd0cc6
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-04-19 23:14:27 -0400

    digitalRoot

--- exercises3.js
+++ exercises3.js
@@ -88,3 +88,25 @@ var findTheDifference = function (s, t) {
         if (m.get(y) < 0) return y; // if added letter is part of s
     }
 };
+
+
+//digitalRoot
+
+function digitalRoot(num) {
+    while (num > 10) {
+        num = digitalRootStep(num);
+    }
+
+    return num;
+}
+
+function digitalRootStep(num) {
+    let root = 0;
+
+    while (num > 0) {
+        root += num % 10;
+        num = Math.floor(num / 10);
+    }
+
+    return root;
+}
