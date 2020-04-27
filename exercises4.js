68f719d0468906313d8e935b06d735fbd3546f17
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-04-26 23:17:16 -0400

    minor fix

--- exercises4.js
+++ exercises4.js
@@ -2,5 +2,5 @@
 
 //The splice() method adds/removes items to/from an array, and returns the removed item(s).
 
-let numbers = [0, 1, 2, 3];
-numbers.splice(1, 0, 4);
+let num1 = [0, 1, 2, 3];
+num1.splice(1, 0, 4);
