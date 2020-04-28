2b1ae384a15bbc8252ff10a7c7d00fc566fdd57c
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-04-27 23:31:00 -0400

    Fib Sum

--- exercises4.js
+++ exercises4.js
@@ -9,3 +9,13 @@ num1.splice(1, 0, 4);
 
 let num2 = [0, 1, 2, 3];
 num2.slice(0, 2);
+
+
+//FibSum
+
+function fibsSum(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    return fibsSum(n - 1) + fibsSum(n - 2) + 1;
+}
