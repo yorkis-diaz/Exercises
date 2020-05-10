e3e74c83376ad4b58c5bf3605b431ede3ecf4204
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-05-09 23:45:12 -0400

    Js Base converter

--- exercises5.js
+++ exercises5.js
@@ -14,3 +14,16 @@ Array.prototype.quickSort = function (cb) {
     dupe.forEach((el) => (cb(el, pivot)) ? leftSide.push(el) : rightSide.push(el));
     return leftSide.quickSort(cb).concat([pivot], rightSide.quickSort(cb))
 }
+
+//base converter
+
+function baseConverter(n, b) {
+    if ([0, 1].includes(n)) return n.toString()
+
+    const digits = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'a', 'b', 'c', 'd', 'e', 'f'
+    ];
+
+    return baseConverter(Math.floor(n / b), b) + digits[n % b];
+}
