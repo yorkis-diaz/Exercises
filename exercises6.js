50a27eb62544f6d5a4bec3cc0e16b182badebf12
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-05-13 22:23:31 -0400

    Js Inherits

---
+++ exercises6.js
@@ -0,0 +1,7 @@
+//inherits 
+Function.prototype.inherits = function (Parent) {
+    function Surrogate() { }
+    Surrogate.prototype = Parent.prototype;
+    this.prototype = new Surrogate();
+    this.prototype.constructor = this;
+};
