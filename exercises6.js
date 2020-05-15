a13c204c284fa61f31595d41c9574d8f28088003
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-05-14 23:17:00 -0400

    JumbleSort

--- exercises6.js
+++ exercises6.js
@@ -5,3 +5,28 @@ Function.prototype.inherits = function (Parent) {
     this.prototype = new Surrogate();
     this.prototype.constructor = this;
 };
+
+
+// jumbleSort
+
+function jumbleSort(str, alphabet = null) {
+    alphabet = alphabet || 'abcdefghijklmnopqrstuvwxyz'.split('');
+    str = str.split('');
+
+    let sorted = false;
+    while (!sorted) {
+        sorted = true;
+        for (let i = 0; i < str.length; i++) {
+            if (i === str.length - 1) break;
+            let current = str[i];
+            let next = str[i + 1];
+            if (alphabet.indexOf(current) > alphabet.indexOf(next)) {
+                str[i] = next;
+                str[i + 1] = current;
+                sorted = false;
+            }
+        }
+    }
+
+    return str.join('');
+}
