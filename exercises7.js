dce6c4b2b8eeca74e6cc1d57d0b405d928c20e7c
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-05-16 23:51:57 -0400

    Js Bind custom:

--- exercises7.js
+++ exercises7.js
@@ -5,3 +5,12 @@
 Function.prototype.myApply = function (context, args = []) {
     return this.bind(context, ...args)();
 };
+
+//mybind
+
+Function.prototype.myBind = function (context, ...bindArgs) {
+    const that = this;
+    return function (...callArgs) {
+        return that.apply(context, bindArgs.concat(callArgs));
+    };
+};
