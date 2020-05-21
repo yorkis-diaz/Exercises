cb934785084208d2ea13b879efdab349411d6f5e
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-05-21 19:12:29 -0400

    Js find custom

--- exercises7.js
+++ exercises7.js
@@ -67,3 +67,12 @@ Array.prototype.myFilter = function (callback) {
 
     return result;
 };
+
+//myfind
+function myFind(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i])) {
+            return array[i];
+        }
+    }
+}
