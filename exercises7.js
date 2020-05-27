92a641ffd47feabec4400728e7e9e645563cf2d0
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-05-26 23:50:15 -0400

    Js Rotate custom

--- exercises7.js
+++ exercises7.js
@@ -144,3 +144,22 @@ function myReverse(array) {
 
     return result;
 }
+
+//myrotate
+
+Array.prototype.myRotate = function (times = 1) {
+    let rotations;
+    const rotated = this.slice(0);
+
+    if (times < 0) {
+        rotations = this.length + (times % this.length);
+    } else {
+        rotations = times % this.length;
+    }
+
+    for (let i = 0; i < rotations; i++) {
+        rotated.push(rotated.shift());
+    }
+
+    return rotated;
+};
