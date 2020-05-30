ab0dc8b8c13033707144de403071cf1ef63ccb28
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-05-29 23:45:11 -0400

    Js Quicksort

--- exercises7.js
+++ exercises7.js
@@ -279,3 +279,24 @@ function isPrime(num) {
 
     return true;
 };
+
+// quicksort
+
+Array.prototype.quickSort = function (func) {
+    if (this.length < 2) return this;
+
+    if (!func) {
+        func = (x, y) => {
+            if (x < y) return - 1;
+            return 1;
+        };
+    }
+
+    const pivot = this[0];
+    let left = this.slice(1).filter((el) => func(el, pivot) === -1);
+    let right = this.slice(1).filter((el) => func(el, pivot) !== -1);
+    left = left.quickSort(func);
+    right = right.quickSort(func);
+
+    return left.concat([pivot]).concat(right);
+};
