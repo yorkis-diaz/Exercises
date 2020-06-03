70f2ea4310a77afaa76f2a908a7806feb673114a
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-06-02 23:44:32 -0400

    Js StringIncludeKey

--- exercises7.js
+++ exercises7.js
@@ -324,3 +324,15 @@ function recSum(nums) {
     if (!nums.length) return 0;
     return nums[0] + recSum(nums.splice(1));
 }
+
+//stringincludekey
+
+function stringIncludeKey(string, key) {
+    if (!key.length) return true;
+
+    let nextKeyChar = key[0];
+    let keyIndex = string.indexOf(nextKeyChar);
+
+    if (keyIndex < 0) return false;
+    return stringIncludeKey(string.slice(keyIndex + 1), key.slice(1));
+}
