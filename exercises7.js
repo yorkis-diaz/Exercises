e48fd92997b1cbebc7065dcd6b4462bd371630e5
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-06-04 23:32:28 -0400

    Js symmetricSubstrings

--- exercises7.js
+++ exercises7.js
@@ -349,3 +349,20 @@ function subsets(arr) {
         return newArr;
     }));
 }
+
+// symmetricSubstrings
+
+String.prototype.symmetricSubstrings = function () {
+    const symmetric = [];
+
+    for (let i = 0; i < this.length; i++) {
+        for (let j = 2; j <= this.length - i; j++) {
+            const subst = this.slice(i, i + j);
+            const reversed = subst.split('').reverse().join('');
+
+            if (subst === reversed) symmetric.push(subst);
+        }
+    }
+
+    return symmetric.sort();
+};
