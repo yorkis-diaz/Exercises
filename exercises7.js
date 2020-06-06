efd64af315bcac024c381f37177736b33f2d29d0
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-06-05 23:58:18 -0400

    Js TwoSum

--- exercises7.js
+++ exercises7.js
@@ -398,3 +398,15 @@ function transpose(arr) {
     return transposedArr;
 }
 
+// twosum
+
+Array.prototype.twoSum = function () {
+    const pairs = [];
+    for (let i = 0; i < this.length - 1; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+            if (this[i] + this[j] === 0) pairs.push([i, j]);
+        }
+    }
+
+    return pairs;
+};
