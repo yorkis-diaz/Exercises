c65843e3b30e5325ce578eb83949c9e79c994126
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-06-14 04:55:11 -0400

    Leetcode Find Common Characters

--- Leetcode_practice_easy.js
+++ Leetcode_practice_easy.js
@@ -17,3 +17,36 @@ var intersect = function (nums1, nums2) {
 
     return intersections
 };
+
+
+// 1002. Find Common Characters
+
+var commonChars = function (A) {
+    let commonLetters = {}
+    let results = []
+
+    for (let i = 0; i < A.length; i++) {
+        let tempObj = {}
+        for (let j = 0; j < A[i].length; j++) {
+            if (i === 0) {
+                if (!commonLetters[A[i][j]]) commonLetters[A[i][j]] = 0
+                commonLetters[A[i][j]]++
+            } else if (i > 0 && commonLetters[A[i][j]]) {
+                if (!tempObj[A[i][j]]) tempObj[A[i][j]] = 0
+                tempObj[A[i][j]]++
+            }
+        }
+        for (let letter in tempObj) {
+            if (commonLetters[letter] < tempObj[letter]) {
+                tempObj[letter] = commonLetters[letter]
+            }
+        }
+        if (i > 0) commonLetters = tempObj
+    }
+
+    for (let common in commonLetters) {
+        const letterArray = new Array(commonLetters[common]).fill(common)
+        results = results.concat(letterArray)
+    }
+    return results
+};
